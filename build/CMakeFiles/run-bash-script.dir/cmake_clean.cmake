46dcc679b94b6da4ad026f6cb5744928c9908c71
Antoine <antoine.gonthier@epitech.eu>
2022-12-23 16:43:16 +0100

    :art: builder

---
+++ build/CMakeFiles/run-bash-script.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run-bash-script"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/run-bash-script.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/run-bash-script.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for run-bash-script.
+# This may be replaced when dependencies are built.
